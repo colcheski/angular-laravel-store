d19e8de98e86de40d584f3775ee77e4f6c203587
Christopher Olcheski <24864308+colcheski@users.noreply.github.com>
2024-06-18 21:41:39 -0400

    add cors support for local development, make sample call to laravel backend through angular as POC

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
     return "<h1>Hello, world!</h1>";
 });
+
+Route::get('/message', function () {
+    return response()->json(['message' => "It's me, da murph!"]);
+});
