1fbadd6078bb6c8120a7d622ae38170e5bff3af9
Christopher Olcheski <24864308+colcheski@users.noreply.github.com>
2024-06-28 16:44:19 -0400

    add interface for angular environment, use api url root from environment for message service call

--- frontend/src/app/message.service.ts
+++ frontend/src/app/message.service.ts
@@ -2,12 +2,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
-  private apiUrl = 'http://localhost:8000/message';
+  private apiUrl = `${environment.apiUrl}/message`;
 
   constructor(private http: HttpClient) {}
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+import { Environment } from "./environment.interface";
+
+export const environment : Environment = {
+    production: true,
+    apiUrl: '' // TBD when we actually start setting up real deployment
+};
