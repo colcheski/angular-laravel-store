ccd4b8c2ba128d7305a234e38ffefd5f82fb4fe8
Christopher Olcheski <24864308+colcheski@users.noreply.github.com>
2024-07-08 09:58:49 -0400

    update home component with user check

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor(public authenticationService: AuthenticationService) {}
+
+  user() {
+    this.authenticationService.user();
+  }
 }
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -34,8 +34,4 @@ export class LoginComponent {
       }
     });
   }
-
-  user() {
-    this.authenticationService.user();
-  }
 }
